593d73083dbf3a97354aeb2df2a52bfe541da94f
Vovan Office <plategi@yandex.ru>
2022-10-26 15:27:13 +0300

    first

---
+++ src/test/java/ru/mainbuilding/api_mainbuilding_v1/ApiMainbuildingV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.mainbuilding.api_mainbuilding_v1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiMainbuildingV1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
