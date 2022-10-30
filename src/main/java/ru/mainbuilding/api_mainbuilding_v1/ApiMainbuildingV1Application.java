a3fce47d0956bdade5970cc335b5265a98fb6fe3
Vovan <plategi@yandex.ru>
2022-10-30 08:17:55 +0300

    home

--- src/main/java/ru/mainbuilding/api_mainbuilding_v1/ApiMainbuildingV1Application.java
+++ src/main/java/ru/mainbuilding/api_mainbuilding_v1/ApiMainbuildingV1Application.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ApiMainbuildingV1Application {
-
 	public static void main(String[] args) {
 		String str = "test in";
 		SpringApplication.run(ApiMainbuildingV1Application.class, args);
